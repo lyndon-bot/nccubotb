fd84e73c03c1175876348624a7e33230587ab0d2
Thomas <tgaines8@eagles.nccu.edu>
2018-03-10 04:37:54 -0600

    chat room

---
+++ Pages/Chat_room.php
@@ -0,0 +1,67 @@
+
+<?php 
+
+include "../Functions/Header.php";
+
+?>
+
+
+<div class="row" style="width:95%; margin-right:auto; margin-left:auto; height:100%;">
+    <div class="col-lg-4 col-md-4 col-sm-4" style=" margin-top:80px;  height:600px; border-right: 3px solid black" class="">
+       
+       <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                     <h2 style=" margin-bottom: 15px"> Other Rooms </h2>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                     <a><h4> Brown's Aquisitions </h4></a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                    <a><h4> Viking's new practice center </h4></a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                    <a><h4> Combine Standouts </h4></a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                    <a><h4> America's Team </h4></a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                    <a><h4> Falcons Fanatics </h4></a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                    <a><h4> Mike Evans' New Deal </h4></a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border-bottom: 2px solid black" class="">
+                    <a><h4> Where will Richard Sherman land? </h4></a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                    <a><p style="align:right"> more </p></a>
+                </div>
+        </div>
+    </div>
+
+    <div class="col-lg-8 col-md-8 col-sm-8" style=" margin-top:80px; margin-bottom: 20px; height:600px;" class="">
+        
+        <div class="col-lg-12 col-md-12 col-sm-12" style=" margin-bottom: 30px; height:400px;" class="">
+
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/5BwCKNggZfc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <textarea name="message" style="width:99%; height:50px; margin-top:10px; margin-left: 5px; margin-right:5px; border-radius: 15px;">
+                </textarea>
+                <button class="btn btn-primary" style="width:15%; margin-top:10px; margin-left:5px"> Chat </button>    
+        </div>
+
+    </div>
+
+</div>
+
+<?php
+include "../Functions/Footer.php";
+?>
