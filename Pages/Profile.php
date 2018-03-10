581d3fd1c086aa7fc106affc7e38d0fcb0722343
Thomas <tgaines8@eagles.nccu.edu>
2018-03-10 06:30:52 -0600

    profile page

---
+++ Pages/Profile.php
@@ -0,0 +1,44 @@
+
+<?php 
+
+include "../Functions/Header.php";
+
+?>
+
+<div class="row" style="width:95%; margin-right:auto; margin-left:auto; height:100%;">
+    <div class="col-lg-6 col-md-6 col-sm-6" style=" margin-top:80px;  height:600px;" class="">
+       
+       <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 " style=" height:400px;" class="">
+                
+                <img src=".../nccubotb/IMG-VID/profilePic.png">
+            </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 " style=" height:400px; width:100%" class="">
+                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu lacinia sapien. Vestibulum eget dui eget leo bibendum pretium nec ultricies velit. Mauris luctus turpis eget nisi iaculis aliquam. Nullam non tellus quis eros elementum tempus. Nunc quis scelerisque neque. Ut venenatis tortor eget leo feugiat, eget facilisis risus sodales. Pellentesque tempus enim tempus nunc lobortis ornare. Donec aliquet finibus lorem non suscipit.</p>
+
+                    <p>Sed risus est, pretium eget tincidunt quis, suscipit et purus. Curabitur tempus sollicitudin eros sit amet tincidunt. Integer interdum arcu ante, in maximus nunc sagittis ut. Aliquam erat volutpat. Fusce a vestibulum leo, et consequat metus. Nam justo ipsum, efficitur id orci a, gravida tristique leo. Nulla eget lorem at erat vehicula viverra et ac mi. Sed elementum, quam et ultrices imperdiet, mi justo gravida massa, convallis elementum lorem leo ac mi. In felis justo, rutrum non lacinia ut, mollis a nulla. Nulla non ipsum risus. Duis finibus tempor posuere.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6" style=" margin-top:80px; margin-bottom: 20px; height:600px;" class="">
+        
+        <div class="col-lg-12 col-md-12 col-sm-12" style=" margin-bottom: 30px; height:400px;" class="">
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu lacinia sapien. Vestibulum eget dui eget leo bibendum pretium nec ultricies velit. Mauris luctus turpis eget nisi iaculis aliquam. Nullam non tellus quis eros elementum tempus. Nunc quis scelerisque neque. Ut venenatis tortor eget leo feugiat, eget facilisis risus sodales. Pellentesque tempus enim tempus nunc lobortis ornare. Donec aliquet finibus lorem non suscipit.</p>
+            <p> Sed risus est, pretium eget tincidunt quis, suscipit et purus. Curabitur tempus sollicitudin eros sit amet tincidunt. Integer interdum arcu ante, in maximus nunc sagittis ut. Aliquam erat volutpat. Fusce a vestibulum leo, et consequat metus. Nam justo ipsum, efficitur id orci a, gravida tristique leo. Nulla eget lorem at erat vehicula viverra et ac mi. Sed elementum, quam et ultrices imperdiet, mi justo gravida massa, convallis elementum lorem leo ac mi. In felis justo, rutrum non lacinia ut, mollis a nulla. Nulla non ipsum risus. Duis finibus tempor posuere.</p>
+            <button class="btn btn-primary" style="width:50%; margin-top:20px;"> Lorem ipsum </button>
+            <button class="btn btn-primary" style="width:50%; margin-top:20px;"> Lorem ipsum </button>     
+        </div>
+
+         <div class="col-lg-12 col-md-12 col-sm-12 " style=" height:400px; width:100%" class="">
+                
+        </div>
+    </div>
+
+</div>
+
+<?php
+include "../Functions/Footer.php";
+?>
