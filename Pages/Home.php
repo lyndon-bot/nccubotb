573ab167479e12958e5a983b973194f246b4ea6c
lyndonbowen <lyndonbowen33@gmail.com>
2018-03-10 06:39:02 -0500

    home Page

--- Pages/Home.php
+++ Pages/Home.php
@@ -6,25 +6,25 @@ include "../Functions/Header.php";
 ?>
 
 <div class="row" style="width:95%; margin-right:auto; margin-left:auto; height:100%;">
-    <div class="col-lg-6 col-md-6 col-sm-6" style=" margin-top:80px;  height:600px;" class="">
+    <div class="col-lg-4 col-md-4 col-sm-4" style=" margin-top:80px;  height:600px;" class="">
        
        <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 " style=" height:400px;" class="">
+                <div class="col-lg-12 col-md-12 col-sm-12 " style="margin-bottom:10px;" class="">
                 
-                <video playsinline autoplay muted poster="polina.jpg" id="" style="height:100%; width:100%;">
+                <video playsinline autoplay muted poster="polina.jpg" id="" style="height:; width:100%;">
                     <source src="../IMG-VID/videoplayback.mp4" type="video/mp4">
                 </video> 
             </div>
 
-                <div class="col-lg-12 col-md-12 col-sm-12 " style=" height:400px; width:100%" class="">
-                    <video playsinline autoplay muted poster="polina.jpg" id="" style="height:100%; width:100%;">
+                <div class="col-lg-12 col-md-12 col-sm-12 " style="  width:100%" class="">
+                    <video playsinline autoplay muted poster="polina.jpg" id="" style="height:; width:100%;">
                         <source src="../IMG-VID/Q&A_ Palestinian-American NFL Player Oday Aboushi.mp4" type="video/mp4">
                     </video> 
             </div>
         </div>
     </div>
 
-    <div class="col-lg-6 col-md-6 col-sm-6" style=" margin-top:80px; margin-bottom: 20px; height:600px;" class="">
+    <div class="col-lg-8 col-md-8 col-sm-8" style=" margin-top:80px; margin-bottom: 20px; height:600px;" class="">
         
         <div class="col-lg-12 col-md-12 col-sm-12" style=" margin-bottom: 30px; height:400px;" class="">
 
