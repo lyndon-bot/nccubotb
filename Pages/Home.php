7e04deddb971a19b2a469ca96ee4b5c590cee863
lyndonbowen <lyndonbowen33@gmail.com>
2018-03-10 01:36:50 -0500

    index and home complete

--- Pages/Home.php
+++ Pages/Home.php
@@ -5,19 +5,38 @@ include "../Functions/Header.php";
 
 ?>
 
-<html>
+<div class="row" style="width:95%; margin-right:auto; margin-left:auto; height:100%;">
+    <div class="col-lg-6 col-md-6 col-sm-6" style=" margin-top:80px;  height:600px;" class="">
+       
+       <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 30px; height:170px;" class="">
 
-<img style="display: block; margin: 0 auto;" src="Logo_NFL.jpg" alt=""/>
 
-</html>
+                </div>
 
-<center>
-<html>
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 30px; height:170px;" class="">
 
-<input type="Submit" name="Submit" value="Submit">
-   
-</html>
-</center>
+
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 30px; height:170px;" class="">
+
+
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-6" style=" margin-top:80px; margin-bottom: 20px; height:600px;" class="">
+        
+        <div class="col-lg-12 col-md-12 col-sm-12" style=" margin-bottom: 30px; height:400px;" class="">
+
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/U_DHpdrx0ZE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <button class="btn btn-primary" style="width:100%; margin-top:20px;"> Vote </button>    
+        </div>
+
+    </div>
+
+</div>
 
 <?php
 include "../Functions/Footer.php";
