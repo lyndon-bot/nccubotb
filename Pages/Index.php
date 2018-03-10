882087d129bea0acf3eafd3e7ca3d65d60376cfd
PUPPYKENS\puppykens25 <imajor@eagles.nccu.edu>
2018-03-09 20:09:29 -0500

    Index and Home update

---
+++ Pages/Index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+
+
+
+<html>
+
+<img style="display: block; margin: 0 auto;" src="Logo_NFL.jpg" alt=""/>
+
+</html>
+
+
+
+
+
+
+
+<center>
+<html>
+
+<input type="Submit" name="Submit" value="Google Signin"><br><br>
+   
+</html>
+
+<a href="Profile.php">Sign up with email</a>
+</center>
+
+
+I might have a solution for the video as background, stretched to the browser-width or height, (but the video will still preserve the aspect ratio, couldnt find a solution for that yet.):
