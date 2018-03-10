a4b4e54fdfb8478637a2b4fc50a6c55f2edbd6ce
Thomas <tgaines8@eagles.nccu.edu>
2018-03-10 02:36:08 -0600

    Player QA page

--- Pages/Player_QA.php
+++ Pages/Player_QA.php
@@ -1,179 +1,66 @@
- <!DOCTYPE html>
-<iframe width="315" height="315" src="https://www.youtube.com/embed/U_DHpdrx0ZE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-<br>
-<br>
 
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.button {
-  border-radius: 4px;
-  background-color: #f4511e;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 28px;
-  padding: 20px;
-  width: 200px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
+<?php 
 
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
+include "../Functions/Header.php";
 
-.button span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
+?>
 
-.button:hover span {
-  padding-right: 25px;
-}
+<div class="row" style="width:95%; margin-right:auto; margin-left:auto; height:100%;">
+    <div class="col-lg-4 col-md-4 col-sm-4" style=" margin-top:80px;  height:600px;" class="">
+       
+       <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px; width:300"></textarea>
+                </div>
 
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-</style>
-</head>
-<body>
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px; width:125"></textarea>
+                </div>
 
-<br><br><br>
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px"></textarea>
+                </div>
 
-<button class="button"><span>NFL Live </span></button>
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px; width:300"></textarea>
+                </div>
 
-</body>
-</html>                                                                                                                                             </button>
-  
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px; width:250"></textarea>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px"></textarea>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px; width:200"></textarea>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px"></textarea>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 panel panel-default" style=" margin-bottom: 5px; height:50px; border:none" class="">
+                     <textarea name="chat" style="border-radius: 15px 50px 30px 5px"></textarea>
+                </div>
+        </div>
+    </div>
+
+    <div class="col-lg-8 col-md-8 col-sm-8" style=" margin-top:80px; margin-bottom: 20px; height:600px;" class="">
+        
+        <div class="col-lg-12 col-md-12 col-sm-12" style=" margin-bottom: 30px; height:400px;" class="">
+
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/5BwCKNggZfc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                <textarea name="message" style="width:99%; height:50px; margin-top:10px; margin-left: 5px; margin-right:5px; border-radius: 15px;">
+                </textarea>
+                <button class="btn btn-primary" style="width:15%; margin-top:10px; margin-left:5px"> Submit </button>    
+        </div>
+
+    </div>
+
 </div>
 
-</body>
-</html>
-
-
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.button {
-  border-radius: 4px;
-  background-color: #f4511e;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 28px;
-  padding: 20px;
-  width: 200px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
-
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-
-.button span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
-
-.button:hover span {
-  padding-right: 25px;
-}
-
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-</style>
-</head>
-<body>
-
-<br><br><br>
-
-<button class="button"><span>Chat </span></button>
-
-</body>
-</html>                                                                                                                                             </button>
-  
-</div>
-
-</body>
-</html>
-
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-.button {
-  border-radius: 4px;
-  background-color: #f4511e;
-  border: none;
-  color: #FFFFFF;
-  text-align: center;
-  font-size: 28px;
-  padding: 20px;
-  width: 200px;
-  transition: all 0.5s;
-  cursor: pointer;
-  margin: 5px;
-}
-
-.button span {
-  cursor: pointer;
-  display: inline-block;
-  position: relative;
-  transition: 0.5s;
-}
-
-.button span:after {
-  content: '\00bb';
-  position: absolute;
-  opacity: 0;
-  top: 0;
-  right: -20px;
-  transition: 0.5s;
-}
-
-.button:hover span {
-  padding-right: 25px;
-}
-
-.button:hover span:after {
-  opacity: 1;
-  right: 0;
-}
-</style>
-</head>
-<body>
-
-<br><br><br>
-
-<button class="button"><span>Player</span><span>Q & A&nbsp</span></button> 
-
-</body>
-</html>                                                                                                                                             </button>
-  
-</div>
-
-</body>
-</html>
+<?php
+include "../Functions/Footer.php";
+?>
