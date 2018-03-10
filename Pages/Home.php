8a53824ac526504f54424eb657453f05db2a0252
Thomas <tgaines8@eagles.nccu.edu>
2018-03-10 05:35:03 -0600

    text spacing vote counter size

--- Pages/Home.php
+++ Pages/Home.php
@@ -39,7 +39,7 @@ include "../Functions/Header.php";
                     };
                 </script>
             <button class="btn btn-primary" style="width:100%; margin-top:20px;" onClick="incr()"> Vote </button>
-            <p>Votes: <a id="clicks">0</a></p>    
+            <h3>Votes: <a id="clicks">0</a></h3>    
         </div>
 
     </div>
