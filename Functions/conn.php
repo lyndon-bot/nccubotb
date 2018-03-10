459ca06e033788622191c2f62c9f13696f554f70
lyndonbowen <lyndonbowen33@gmail.com>
2018-03-09 20:58:54 -0500

    Database Connect

---
+++ Functions/conn.php
@@ -0,0 +1,11 @@
+<?php 
+
+$server = "localhost";
+$user = "root";
+$password = "";
+$DB = "NFLCPDB";
+
+$conn = mysqli_connect("$server", "$user", "$password", "$DB") or die("cannot connect");
+
+
+?>
