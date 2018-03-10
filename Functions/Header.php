ea986dc8eb3acba83c2deb04ad4192273ba3b289
Thomas <tgaines8@eagles.nccu.edu>
2018-03-09 19:37:25 -0600

    header change2

--- Functions/Header.php
+++ Functions/Header.php
@@ -17,10 +17,9 @@
     <nav class="navbar navbar-light bg-light">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
             <a class="navbar-brand" href="#"> websitename </a>
-            <ul class="nav navbar-nav navbar-right">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">Page 1</a></li>
-                <li><a href="#">Page 2</a></li>
+            <ul class="nav navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
             </ul>
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
